b0250b73a1bdee80b8e3adf1987221039e30f5f3
Adriano J. Holanda <ajholanda@gmail.com>
2017-03-24 10:02:53 -0300

    initial commit

---
+++ lobby.py
@@ -0,0 +1,32 @@
+from igraph import *
+
+def lobby(graph):
+    lobbies = []
+
+    for u in self.graph.vs:
+        lobby = 1 # initialize lobby
+        vdegs = [] # neighbors' degree
+
+        edges = self.graph.es.select(_source=u.index)
+        for e in edges:
+            v = self.graph.vs[e.target]
+            vdegs.append(v.degree(mode='out'))
+                            
+	vdegs.sort()
+        vdegs.reverse()
+	old_vdeg = -1
+	idx = 0
+        for vdeg in vdegs:
+            # Ignore repetition of degrees
+            if (vdeg != old_vdeg):
+                idx = idx + 1
+            	if (vdeg <= idx):
+                    u['lobby'] = lobby = vdeg
+                    break
+            old_deg = vdeg
+
+	    lobbies.append(lobby)
+
+    return lobbies
+            
+            
