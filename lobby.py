6a1fd8b86075081124c4eb807e7e59b52c71bb8e
Adriano J. Holanda <ajholanda@gmail.com>
2017-03-30 10:25:41 -0300

    clean the algorithm

--- lobby.py
+++ lobby.py
@@ -4,29 +4,31 @@ def lobby(graph):
     lobbies = []
 
     for u in graph.vs:
-        lobby = 1 # initialize lobby
-        vdegs = [] # neighbors' degree
+        print(u['name'] + "\tdegree=" + str(u.degree(mode='out')))
+
+        u['lobby'] = lobby = 1 # initialize lobby
+        degs = [] # neighbors' degree
 
         edges = graph.es.select(_source=u.index)
         for e in edges:
             v = graph.vs[e.target]
-            vdegs.append(v.degree(mode='out'))
+            degs.append(v.degree(mode='out'))
                             
-	vdegs.sort()
-        vdegs.reverse()
-	old_vdeg = -1
-	idx = 0
-        for vdeg in vdegs:
-            # Ignore repetition of degrees
-            if (vdeg != old_vdeg):
-                idx = idx + 1
-            	if (vdeg <= idx):
-                    u['lobby'] = lobby = vdeg
-                    break
-            old_deg = vdeg
+	degs.sort()
+        degs.reverse()
+	old_idx = idx = 0
+        for deg in degs:
+            lobby = idx = idx + 1
+            print("\t" + str(idx) + "\t" + str(deg))
 
-	    lobbies.append(lobby)
+            if (deg < idx):
+                u['lobby'] = lobby = old_idx
+                break
+            old_idx = idx
+
+        print("lobby=" + str(lobby))
+
+        lobbies.append(lobby)
 
     return lobbies
-            
             
