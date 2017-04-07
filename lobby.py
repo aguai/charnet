7746f4556013af16eed5f66cc549be5f705db8dd
Adriano J. Holanda <ajholanda@gmail.com>
2017-04-07 13:27:56 -0300

    add comments

--- lobby.py
+++ lobby.py
@@ -1,5 +1,23 @@
 from igraph import *
 
+"""Lobby or h index
+================
+
+All graph vertices are traversed and Lobby index is calculated and stored in the lobby macro-field.
+
+If a node has the following list of neighbors sorted by degree:
+
+==========  ========
+neighbor     degree
+==========  ========
+   1          21 
+   2          18 
+   3           4 
+   4           3 
+==========  ========
+   
+the Lobby index is 3 because degree $\leq$ neighbor_position. Degree repetitions are not accounted.
+"""
 def lobby(G, log_file=None):
     lobbies = []
 
