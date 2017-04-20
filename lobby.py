c6486105d9377cbb273f6057a4707ecacf2a2663
Adriano J. Holanda <ajholanda@gmail.com>
2017-04-20 14:15:02 -0300

    fix identation

--- lobby.py
+++ lobby.py
@@ -1,25 +1,25 @@
 from igraph import *
 
 def lobby(G, log_file=None):
-"""Lobby or h index
-================
+    """Lobby or h index
+    ================
+    
+    All graph vertices are traversed and Lobby index is calculated and stored in the lobby macro-field.
+    
+    If a node has the following list of neighbors sorted by degree:
 
-All graph vertices are traversed and Lobby index is calculated and stored in the lobby macro-field.
-
-If a node has the following list of neighbors sorted by degree:
-
-==========  ========
-neighbor     degree
-==========  ========
-   1          21 
-   2          18 
-   3           4 
-   4           3 
-==========  ========
-   
-the Lobby index is 3 because degree $\leq$ neighbor_position. 
-
-"""
+    ==========  ========
+    neighbor     degree
+    ==========  ========
+    1          21 
+    2          18 
+    3           4 
+    4           3 
+    ==========  ========
+    
+    the Lobby index is 3 because degree $\leq$ neighbor_position. 
+    
+    """
     lobbies = []
 
     if (log_file!=None):
